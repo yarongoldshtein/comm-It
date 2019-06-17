3324adaa38ba37cc4046514c614c4e9e2909d2ba
yarongoldshtein <yaron.goldshtein@gmail.com>
2019-06-18 01:08:41 +0300

    init

---
+++ src/main/java/com/server/commIt/CommItApplication.java
@@ -0,0 +1,13 @@
+package com.server.commIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommItApplication.class, args);
+	}
+
+}
